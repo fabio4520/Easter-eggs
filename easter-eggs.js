d68443b6fe17c46bdab4391af5868544aacd5309
Denis Nahuamel <denis123nahuamel@github.com>
2022-05-12 17:27:50 -0500

    ADD: function loader

--- easter-eggs.js
+++ easter-eggs.js
@@ -2,6 +2,16 @@ function TicTacToe() {
 }
 
 function Loader() {
+  array = ["|", "/", "-", "\'"];
+  i = 0;
+  let timerId = setInterval(()=> {
+    if (i == (array.length-1)) i = 0; 
+    console.clear();
+    console.log(array[i]);
+    i++;
+    
+  },300);
+  setTimeout(() => { clearInterval(timerId); }, 3000);
 }
 
 function Clock() {
